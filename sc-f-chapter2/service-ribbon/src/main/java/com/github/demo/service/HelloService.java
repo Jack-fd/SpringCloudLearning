69afa860c98f031ba5b3f94ffd929cbfcd4b47fd
MQ <mqiang@ruijie.com.cn>
2018-09-21 11:16:17 +0800

    first commit

---
+++ sc-f-chapter2/service-ribbon/src/main/java/com/github/demo/service/HelloService.java
@@ -0,0 +1,21 @@
+package com.github.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+    }
+
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+}
